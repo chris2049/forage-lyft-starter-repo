b478c49b3020fd0c7bf4819da74511a33e70a696
chris2049 <3ccortez@gmail.com>
2023-06-08 13:27:39 -0700

    add static method

--- carFactory.py
+++ carFactory.py
@@ -3,30 +3,35 @@ from engine.engines import CapuletEngine, SternmanEngine, WilloughbyEngine
 from battery.batteries import NubbinBattery, SpindlerBattery
 
 class CarFactory():
+    @staticmethod
     def  create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine( current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
 
         return Car(engine, battery)
 
+    @staticmethod
     def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(current_date, last_service_date)
 
         return Car(engine, battery)
     
+    @staticmethod
     def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(current_date, last_service_date)
         
         return Car(engine, battery)
     
+    @staticmethod
     def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(current_date, last_service_date)
         
         return Car(engine, battery)
     
+    @staticmethod
     def create_pallindrome(current_date, last_service_date, warning_light_is_on):
         engine = SternmanEngine(warning_light_is_on)
         battery = SpindlerBattery(current_date, last_service_date)
